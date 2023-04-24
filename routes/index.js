f5882bda54ef362199dd76666a31cac7dd101d48
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-04-24 13:27:18 +0100

    feat: enable index route to query documents count

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 let express = require("express");
 let router = express.Router();
 
+const Player = require("../models/player");
+const Club = require("../models/club");
+const Position = require("../models/position");
+
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.get("/", async function (req, res) {
+  await Promise.all([
+    Player.countDocuments({}),
+    Club.countDocuments({}),
+    Position.countDocuments({}),
+  ])
+    .then((results) => {
+      player_count = results[0];
+      club_count = results[1];
+      position_count = results[2];
+      const data = { player_count, club_count, position_count };
+      res.render("index", {
+        title: "My Home Page",
+        data,
+        error: "",
+      });
+    })
+    .catch((error) => {
+      res.render("index", {
+        title: "My Home Page",
+        error,
+      });
+    });
 });
 
 module.exports = router;
