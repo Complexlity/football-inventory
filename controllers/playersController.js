69c932f1421796d37ceaff98da4b20d16471dd28
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-04-24 18:15:24 +0100

    feat: create players controller index function

--- controllers/playersController.js
+++ controllers/playersController.js
@@ -1,3 +1,15 @@
-exports.index = (req, res) => {
-  res.render("players", { title: "Players Page" });
+const Player = require("../models/player");
+
+exports.index = async (req, res) => {
+  let error = "";
+  let players = "";
+  try {
+    players = await Player.find({}).populate({
+      path: "club position",
+      select: "name -_id",
+    });
+  } catch (err) {
+    error = err;
+  }
+  res.render("players", { title: "Players Page", players, error });
 };
