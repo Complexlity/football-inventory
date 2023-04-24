faf379dd83ac6a7e312544a758f1c190c9c1c40c
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-04-24 16:17:58 +0100

    feat: add clubs page

--- controllers/clubsController.js
+++ controllers/clubsController.js
@@ -1,3 +1,14 @@
-exports.index = (req, res) => {
-  res.render("clubs", { title: "Clubs Home Page" });
+const Club = require("../models/club");
+
+exports.index = async (req, res) => {
+  let error = "";
+  let clubs = {};
+  // Find all clubs
+  try {
+    clubs = await Club.find({}).sort({ name: "asc" });
+    console.log(clubs);
+  } catch (err) {
+    error = err;
+  }
+  res.render("clubs", { title: "Clubs Home Page", clubs, error });
 };
