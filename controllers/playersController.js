3226f14fe1824994659244fd4561af325b3d013d
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-05-04 14:30:40 +0100

    add validation to player create and uppdate controller

--- controllers/playersController.js
+++ controllers/playersController.js
@@ -1,6 +1,7 @@
 const Player = require("../models/player");
 const Club = require("../models/club");
 const Position = require("../models/position");
+const { body, validationResult } = require("express-validator");
 
 function filterItems(filtered, filter) {
   const filteredPositions = [...filtered];
@@ -10,6 +11,29 @@ function filterItems(filtered, filter) {
   filteredPositions.splice(index, 1)[0];
   // filteredPositions.unshift(removed);
   return filteredPositions;
+}
+
+function playerValidationChain() {
+  return [
+    body("playername")
+      .trim()
+      .isLength({ min: 1, max: 10 })
+      .withMessage("Player name must be between 1 and 10 characters"),
+    body("age")
+      .trim()
+      .isInt({ min: 1 })
+      .withMessage("Age must be a positive integer"),
+    body("position").trim().isString().withMessage("Position must be a string"),
+    body("club").trim().isString().withMessage("Club must be a string"),
+    body("price")
+      .trim()
+      .isInt({ min: 0 })
+      .withMessage("Price must be a positive integer"),
+    body("forsale")
+      .trim()
+      .isIn(["0", "1"])
+      .withMessage("For sale must be either 0 or 1"),
+  ];
 }
 
 exports.index = async (req, res) => {
@@ -68,46 +92,53 @@ exports.create_get = async (req, res) => {
   });
 };
 
-exports.create_post = async (req, res) => {
-  let clubId, positionId, club;
-  const name = req.body.playername;
-  const age = req.body.age;
-  const positionAbbr = req.body.position;
-  const clubName = req.body.club;
-  const price = req.body.price;
-  const forSale = req.body.forsale === "1";
+exports.create_post = [
+  playerValidationChain(),
+  async (req, res) => {
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    let clubId, positionId, club;
+    const name = req.body.playername;
+    const age = req.body.age;
+    const positionAbbr = req.body.position;
+    const clubName = req.body.club;
+    const price = req.body.price;
+    const forSale = req.body.forsale === "1";
 
-  //------------------------
-  // VALIDATE INPUTS HERE
-  //------------------------
-  await Promise.all([
-    Club.findOne({ name: clubName }),
-    Position.findOne({ abbr: positionAbbr }),
-  ])
-    .then(async (result) => {
-      // return;
-      club = result[0];
-      clubId = club._id;
-      positionId = result[1]._id;
-      console.log("I am here");
+    //------------------------
+    // VALIDATE INPUTS HERE
+    //------------------------
+    await Promise.all([
+      Club.findOne({ name: clubName }),
+      Position.findOne({ abbr: positionAbbr }),
+    ])
+      .then(async (result) => {
+        // return;
+        club = result[0];
+        clubId = club._id;
+        positionId = result[1]._id;
+        console.log("I am here");
 
-      const newPlayer = new Player({
-        name,
-        age,
-        position: positionId,
-        club: clubId,
-        price,
-        forSale,
+        const newPlayer = new Player({
+          name,
+          age,
+          position: positionId,
+          club: clubId,
+          price,
+          forSale,
+        });
+        club.players.push(newPlayer);
+        await Promise.all([newPlayer.save(), club.save()]);
+        return res.redirect(`/clubs/${clubId}`);
+      })
+
+      .catch((err) => {
+        res.redirect(`/players/create`);
       });
-      club.players.push(newPlayer);
-      await Promise.all([newPlayer.save(), club.save()]);
-      return res.redirect(`/clubs/${clubId}`);
-    })
-
-    .catch((err) => {
-      res.redirect(`/players/create`);
-    });
-};
+  },
+];
 
 exports.update_get = async (req, res) => {
   let playerId = req.params.id;
@@ -147,50 +178,61 @@ exports.update_get = async (req, res) => {
   // res.json({ filteredClubs, filteredPositions });
 };
 
-exports.update_post = async (req, res) => {
-  let player, newClub, position, positionId, clubId, oldClub;
-  const id = req.params.id;
-  const {
-    playername: name,
-    age,
-    position: abbr,
-    club,
-    price,
-    forsale,
-  } = req.body;
-  //------------------------
-  // VALIDATE INPUTS HERE
-  //------------------------
+exports.update_post = [
+  playerValidationChain(),
+  async (req, res) => {
+    console.log("I am here");
 
-  await Promise.all([
-    Player.findOne({ _id: id }),
-    Position.findOne({ abbr }),
-    Club.findOne({ name: club }),
-  ]).then(async (result) => {
-    player = result[0];
-    positionId = result[1]._id;
-    newClub = result[2];
-    clubId = newClub._id;
-
-    // Check if old and new club are not the same. Deletes player from old club and adds player to new club
-    if (player.club !== clubId) {
-      oldClub = await Club.findOne({ _id: player.club });
-      oldClub.players = oldClub.players.filter((value) => value !== player._id);
-      newClub.players.push(player._id);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+    let player, newClub, positionId, clubId, oldClub;
+    const id = req.params.id;
+    const {
+      playername: name,
+      age,
+      position: abbr,
+      club,
+      price,
+      forsale,
+    } = req.body;
+    //------------------------
+    // VALIDATE INPUTS HERE
+    //------------------------
 
-    // Assign player to newly updated values
-    player.name = name;
-    player.age = age;
-    player.club = clubId;
-    player.position = positionId;
-    player.price = price;
-    player.forSale = forsale === "1";
+    await Promise.all([
+      Player.findOne({ _id: id }),
+      Position.findOne({ abbr }),
+      Club.findOne({ name: club }),
+    ]).then(async (result) => {
+      player = result[0];
+      positionId = result[1]._id;
+      newClub = result[2];
+      clubId = newClub._id;
 
-    await Promise.all([player.save(), newClub.save(), oldClub.save()]);
-    res.redirect("/players");
-  });
-};
+      // Check if old and new club are not the same. Deletes player from old club and adds player to new club
+      if (player.club !== clubId) {
+        oldClub = await Club.findOne({ _id: player.club });
+        oldClub.players = oldClub.players.filter(
+          (value) => value !== player._id
+        );
+        newClub.players.push(player._id);
+      }
+
+      // Assign player to newly updated values
+      player.name = name;
+      player.age = age;
+      player.club = clubId;
+      player.position = positionId;
+      player.price = price;
+      player.forSale = forsale === "1";
+
+      await Promise.all([player.save(), newClub.save(), oldClub.save()]);
+      res.redirect("/players");
+    });
+  },
+];
 
 exports.delete_post = async (req, res) => {
   const id = req.params.id;
